3f9cbfa4509a2af5d8ff3a5afd17644647d31feb
Kevin Hernandez <KFHEDU@gmail.com>
2025-02-11 15:44:32 -0800

    Adding listview of Location History

--- lib/widgets/location_tab_widget.dart
+++ lib/widgets/location_tab_widget.dart
@@ -4,14 +4,134 @@ import 'package:weatherapp/scripts/location.dart' as location;
 
 // TODO: When the user clicks the Get From Location button:
 // Besides setting the active location (which it is already doing)
-// Add that location to a saved loations list (you'll need to create this)
+// Add that location to a saved locations list (you'll need to create this)
 // Display the saved locations in a new widget below the get from GPS button
 // make the saved location widgets onTap()-able, so the user can tap a previously saved location, 
 // setting the location based on that
-// 
+//
 
+class LocationHistoryWidget extends StatelessWidget {
+  const LocationHistoryWidget({
+    super.key
+  });
 
-class LocationTabWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300.0,
+      height: 500.0,
+      child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 50,
+              child: const Center(child:
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center ,//Center Row contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+                  children: [
+                  Icon(Icons.location_on),
+                  SizedBox(width: 10),
+                  Text("New York, New York 10007", style: TextStyle(fontWeight: FontWeight.bold)),
+              ],)),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ]
+        )
+    );
+  }
+}
+
+class LocationTabWidget extends StatefulWidget {
   const LocationTabWidget({
     super.key,
     required Function setLocation,
@@ -22,12 +142,19 @@ class LocationTabWidget extends StatelessWidget {
   final location.Location? _location;
 
   @override
+  State<LocationTabWidget> createState() => _LocationTabWidgetState();
+}
+
+class _LocationTabWidgetState extends State<LocationTabWidget> {
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        LocationDisplayWidget(activeLocation: _location),
-        LoctionInputWidget(setLocation: _setLocation),
-        ElevatedButton(onPressed: ()=>{_setLocation()},child: const Text("Get From GPS"))
+        LocationDisplayWidget(activeLocation: widget._location),
+        LoctionInputWidget(setLocation: widget._setLocation),
+        ElevatedButton(onPressed: ()=>{widget._setLocation()},child: const Text("Get From GPS")),
+        SizedBox(height: 20),
+        LocationHistoryWidget()
       ],
     );
   }
